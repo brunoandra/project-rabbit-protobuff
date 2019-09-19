3e1added1b0936ab702febac2af89e104168ce62
Bruno Barros <bruno.barros@zup.com.br>
2019-09-19 10:42:06 -0300

    arrumando as dependencias

--- project-rabbit-protobuff-service/src/main/java/br/com/metsys/rabbitprotobuff/pessoa/ProjectRabbitProtobuffApp.java
+++ project-rabbit-protobuff-service/src/main/java/br/com/metsys/rabbitprotobuff/pessoa/ProjectRabbitProtobuffApp.java
@@ -4,10 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "br.com.metsys.rabbitprotobuff")
-@EnableFeignClients(basePackages = "br.com.metsys.rabbitprotobuff")
 public class ProjectRabbitProtobuffApp extends Application {
 
     public static void main(String[] args) {
