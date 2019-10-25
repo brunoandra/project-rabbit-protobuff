045a308b5a2bca8506de2a3bf410e2039f75eaa9
Bruno Barros <bruno.barros@zup.com.br>
2019-10-24 23:38:01 -0300

    implementando controller de criação de pessoa

---
+++ project-rabbit-protobuff-client/src/main/java/br/com/metsys/rabbitprotobuff/pessoa/CreatePersonApi.java
@@ -0,0 +1,17 @@
+package br.com.metsys.rabbitprotobuff.pessoa;
+
+import br.com.metsys.agendaaqui.pessoa.exception.UseCaseException;
+import br.com.metsys.rabbitprotobuff.pessoa.model.CreatePersonRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@RequestMapping("person")
+public interface CreatePersonApi {
+    @PostMapping
+    ResponseEntity<Long> execute(@RequestBody CreatePersonRequest createPersonRequest, @RequestHeader Map<String, String> headers) throws UseCaseException;
+}
